342b60073597ec773341429e8422401a4fc7bec2
harshadkhatane <91969422+harshadkhatane@users.noreply.github.com>
2022-02-26 10:55:11 +0530

    Add files via upload

---
+++ calc.java
@@ -0,0 +1,10 @@
+public class calc
+{
+    public static void main(String[] args) {
+        int a,b;
+        a=20;
+        b=15;
+        int c=a+b;
+        System.out.println("sumation of two variable"+c);    
+    }
+}
